bb51bb1212cbf7f69107cb23b0519bc17ff00fec
yakitama5 <rittitarou@yahoo.co.jp>
2025-02-24 14:32:16 +0900

    🎨design: ホームボタンのレイアウト作成

--- apps/app/lib/src/home/pages/home_page.dart
+++ apps/app/lib/src/home/pages/home_page.dart
@@ -1,20 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/i18n/strings.g.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class HomePage extends ConsumerWidget {
-  const HomePage({
-    super.key,
-  });
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(i18n.app.homePage.title),
+      body: const Center(child: Text('Hello Home!')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
       ),
-      body: const Center(
-        child: Text('Hello Home!'),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endContained,
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          spacing: 8,
+          children: [
+            IconButton(onPressed: () {}, icon: const Icon(Icons.settings)),
+            IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+            IconButton(onPressed: () {}, icon: const Icon(Icons.notifications)),
+          ],
+        ),
       ),
     );
   }
