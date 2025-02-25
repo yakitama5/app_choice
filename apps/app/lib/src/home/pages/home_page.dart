bee353554b09398b6a6fbd4375802082475f1565
yakitama5 <rittitarou@yahoo.co.jp>
2025-02-25 16:58:06 +0900

    🎨design: ホーム画面のレイアウト作成

--- apps/app/lib/src/home/pages/home_page.dart
+++ apps/app/lib/src/home/pages/home_page.dart
@@ -8,17 +8,41 @@ class HomePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-      body: const Center(child: Text('Hello Home!')),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.add),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endContained,
-      bottomNavigationBar: HomeBottomAppBar(
-        onSettingsPressed: () => const SettingPageRoute().go(context),
-        onSearchPressed: () {},
-        onNotificationsPressed: () {},
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        body: const CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              title: TabBar(
+                tabs: [
+                  Tab(icon: Icon(Icons.person), child: Text('わたしの投稿')),
+                  Tab(icon: Icon(Icons.trending_up), child: Text('トレンド')),
+                  Tab(icon: Icon(Icons.timer), child: Text('最新')),
+                ],
+              ),
+            ),
+            SliverFillRemaining(
+              child: TabBarView(
+                children: [
+                  Center(child: Text('わたしの投稿')),
+                  Center(child: Text('トレンド')),
+                  Center(child: Text('最新')),
+                ],
+              ),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.add),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endContained,
+        bottomNavigationBar: HomeBottomAppBar(
+          onSettingsPressed: () => const SettingPageRoute().go(context),
+          onSearchPressed: () {},
+          onNotificationsPressed: () {},
+        ),
       ),
     );
   }
